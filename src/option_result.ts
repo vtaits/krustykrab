e16232e99ebaeb74ee4ab35a56ed3e4d9de04147
Vadim Taits <taizvadim@gmail.com>
2024-01-15 01:43:00 +0300

    WIP: docs

--- src/option_result.ts
+++ src/option_result.ts
@@ -4,28 +4,158 @@
  * https://doc.rust-lang.org/std/result/enum.Result.html
  */
 export type Result<R, E> = Readonly<{
+	/**
+	 * ```ts
+	 * Ok('foo').isOk(); // returns `true`
+	 * Err('foo').isOk(); // returns `false`
+	 * ```
+	 */
 	isOk: () => boolean;
+	/**
+	 * ```ts
+	 * Ok(123).isOkAnd((value) => value > 100); // returns `true`
+	 * Ok(12).isOkAnd((value) => value > 100); // returns `false`
+	 * Err(123).isOkAnd((value) => value > 100); // returns `false`
+	 * ```
+	 */
 	isOkAnd: (fn: (arg: R) => boolean) => boolean;
+	/**
+	 * ```ts
+	 * Ok('foo').isErr(); // returns `false`
+	 * Err('foo').isErr(); // returns `true`
+	 * ```
+	 */
 	isErr: () => boolean;
+	/**
+	 * ```ts
+	 * Ok(123).isErrAnd((value) => value > 100); // returns `false`
+	 * Err(123).isErrAnd((value) => value > 100); // returns `true`
+	 * Err(12).isErrAnd((value) => value > 100); // returns `false`
+	 * ```
+	 */
 	isErrAnd: (fn: (arg: E) => boolean) => boolean;
+	/**
+	 * ```ts
+	 * Ok('foo').ok(); // returns `Some('foo')`
+	 * Err('foo').ok(); // returns `None()`
+	 * ```
+	 */
 	ok: () => Option<R>;
+	/**
+	 * ```ts
+	 * Ok('foo').err(); // returns `None()`
+	 * Err('foo').err(); // returns `Some('foo')`
+	 * ```
+	 */
 	err: () => Option<E>;
+	/**
+	 * ```ts
+	 * Ok(1).map((value) => value * 2); // returns `Ok(2)`
+	 * Err(1).map((value) => value * 2); // returns `Err(1)`
+	 * ```
+	 */
 	map: <U>(fn: (arg: R) => U) => Result<U, E>;
+	/**
+	 * ```ts
+	 * Ok(1).mapOr(10, (value) => value * 2); // returns `2`
+	 * Err(1).mapOr(10, (value) => value * 2); // returns `10`
+	 * ```
+	 */
 	mapOr: <U>(defaultValue: U, fn: (arg: R) => U) => U;
+	/**
+	 * ```ts
+	 * Ok(1).mapOrElse((err) => err * 10, (value) => value * 2); // returns `2`
+	 * Err(1).mapOrElse((err) => err * 10, (value) => value * 2); // returns `10`
+	 * ```
+	 */
 	mapOrElse: <U>(getDefaultValue: (arg: E) => U, fn: (arg: R) => U) => U;
+	/**
+	 * ```ts
+	 * Ok(1).mapErr((err) => err * 2); // returns `Ok(1)`
+	 * Err(1).mapErr((err) => err * 2); // returns `Err(2)`
+	 * ```
+	 */
 	mapErr: <F>(fn: (arg: E) => F) => Result<R, F>;
+	/**
+	 * ```ts
+	 * Ok('foo').expect('error message'); // returns `'foo'`
+	 * Err('foo').expect('error message'); // throws `new Error('error message')`
+	 * ```
+	 */
 	expect: (msg: string) => R;
+	/**
+	 * ```ts
+	 * Ok('foo').expectErr('error message'); // throws `new Error('error message')`
+	 * Err('foo').expectErr('error message'); // returns `'foo'`
+	 * ```
+	 */
 	expectErr: (msg: string) => E;
+	/**
+	 * ```ts
+	 * Ok('foo').unwrap(); // returns `'foo'`
+	 * Err('foo').unwrap(); // throws
+	 * ```
+	 */
 	unwrap: () => R;
+	/**
+	 * ```ts
+	 * Ok('foo').unwrapErr(); // throws
+	 * Err('foo').unwrapErr(); // returns `'foo'`
+	 * ```
+	 */
 	unwrapErr: () => E;
+	/**
+	 * ```ts
+	 * Ok('foo').unwrapOr('bar'); // returns `'foo'`
+	 * Err('foo').unwrapOr('bar'); // returns `'bar'`
+	 * ```
+	 */
 	unwrapOr: (defaultValue: R) => R;
+	/**
+	 * ```ts
+	 * Ok(1).unwrapOrElse('bar'); // returns `1`
+	 * Err(1).unwrapOrElse((err) => err * 2); // returns `2`
+	 * ```
+	 */
 	unwrapOrElse: (getDefaultValue: (arg: E) => R) => R;
+	/**
+	 * ```ts
+	 * Ok('foo').and(otherResult); // returns `otherResult`
+	 * Err('foo').and(otherResult); // returns `Err('foo')`
+	 * ```
+	 */
 	and: <U>(res: Result<U, E>) => Result<U, E>;
+	/**
+	 * ```ts
+	 * Ok(1).andThen((value) => Ok(value * 2)); // returns `Ok(2)`
+	 * Ok(1).andThen((value) => Err(value * 2)); // returns `Err(2)`
+	 * Err(1).andThen((value) => Ok(value * 2)); // returns `Err(1)`
+	 * ```
+	 */
 	andThen: <U>(getRes: (arg: R) => Result<U, E>) => Result<U, E>;
+	/**
+	 * ```ts
+	 * Ok('foo').or(otherResult); // returns `Ok('foo').`
+	 * Err('foo').or(otherResult); // returns `otherResult`
+	 * ```
+	 */
 	or: <F>(res: Result<R, F>) => Result<R, F>;
+	/**
+	 * ```ts
+	 * Ok(1).orElse((value) => Ok(value * 2)); // returns `Ok(1)`
+	 * Err(1).orElse((value) => Ok(value * 2)); // returns `Ok(2)`
+	 * Err(1).orElse((value) => Err(value * 2)); // returns `Err(2)`
+	 * ```
+	 */
 	orElse: <F>(getRes: (arg: E) => Result<R, F>) => Result<R, F>;
 }>;
 
+/**
+ * @param result The success value
+ * @returns The success result
+ *
+ * https://doc.rust-lang.org/std/result/enum.Result.html#variant.Ok
+ */
 export function Ok<R, E>(result: R): Result<R, E> {
 	const self: Result<R, E> = {
 		isOk: () => true,
@@ -57,6 +187,12 @@ export function Ok<R, E>(result: R): Result<R, E> {
 	return self;
 }
 
+/**
+ * @param err The error value
+ * @returns The error result
+ *
+ * https://doc.rust-lang.org/std/result/enum.Result.html#variant.Err
+ */
 export function Err<R, E>(err: E): Result<R, E> {
 	const self: Result<R, E> = {
 		isOk: () => false,
